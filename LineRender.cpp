02d702f06feec35a88ab56d8bef3ad33c427e79e
Godwin Stephens <godwin.skye@gmail.com>
2017-01-18 02:19:58 -0800

    Very early prototype/high level view

---
+++ LineRender.cpp
@@ -0,0 +1,5 @@
+#include "LineRender.h"
+
+void LineRenderer::DDArender(int x1, int y1, int x2, int y2) {
+
+}
---
+++ LineRender.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+
+namespace LineRenderer {
+	void DDArender(int x1, int y1, int x2, int y2);
+}
